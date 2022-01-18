d7b2cd15c7a8dc485035eec43595ddb99e786cc8
Jed <vjeudy@protonmail.com>
2022-01-18 22:17:26 +0100

    wip: formats and prints current weather

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::{fs};
-use std::io::{BufReader, BufRead};
-use fs::{File};
-use regex::Regex;
 // use serde::{Serialize, Deserialize};
 
 mod structs;
+mod read_env;
+
+use read_env::get_apikey;
+use structs::ApiResponse;
+use colored::Colorize;
 
 const OPEN_WEATHER_URL: &str = "https://api.openweathermap.org/data/2.5";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get env file content
-    let api_key = read_env_file("OPEN_WEATHER".to_string());
+    let api_key = get_apikey("OPEN_WEATHER".to_string());
 
     let city = "lyon".to_string();
 
@@ -19,38 +20,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let body: structs::ApiResponse = reqwest::get(url).await?
         .json().await?;
-        // .json::<HashMap<String, String>>().await?;
-        // .json::<HashMap<String, String>>()
-        // .await?;
 
-        println!("body {:?}", body);
+    let res = print_weather(body);
 
     Ok(())
 }
 
+fn print_weather (current: ApiResponse) {
+    println!(
+        r#"
+        |{}| {}
+        |{}| {}
+        |{}| {}
+        |{}| lon {}, lat {}
+        "#,
+        green("City"),
+        // format!("City").bold().green(),
+        current.name,
+        green("Weather"),
+        &current.weather.first().unwrap().description,
+        green("Temperature"),
+        current.main.temp,
+        green("Coordinates"),
+        current.coord.lon,
+        current.coord.lat,
+    );
 
-fn read_env_file (env_key: String) -> String {
-    let env_file = File::open(".env")
-        .expect("Error when opening .env file");
-
-    let reader = BufReader::new(env_file);
-    let formatted_regex = format!(r"^{}", env_key);
-    let regex = Regex::new(&formatted_regex)
-        .expect("Regex could not be generated");
-
-    let lines = reader
-        .lines()
-        .map(|l| l.unwrap());
-
-    lines
-        // .find(|line| regex.is_match(&line)).unwrap();
-        .fold(String::new(), |acc, line| {
-            if regex.is_match(&line) {
-                let splitted: Vec<&str> = line.split('=').collect();
-
-                return splitted[1].to_string();
-            }
-
-            acc
-        })
+    fn green (name: &str) -> String {
+        format!("{}", name).bold().green().to_string()
+    }
 }
